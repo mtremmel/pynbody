47e4f171b01d843a76e2ed1bd215be7ec55ffda5
Michael Tremmel <mjt29@astro.washington.edu>
2015-08-17 12:20:33 -0700

    added "qkey" parameter to velocity_image in sph to make the velocity label optional (True/False)

--- pynbody/plot/sph.py
+++ pynbody/plot/sph.py
@@ -50,7 +50,7 @@ def faceon_image(sim, *args, **kwargs):
 
 def velocity_image(sim, width="10 kpc", vector_color='black', edgecolor='black',
                    vector_resolution=40, scale=None, mode='quiver', key_x=0.3, key_y=0.9,
-                   key_color='white', key_length="100 km s**-1", density=1.0, **kwargs):
+                   qkey=True,key_color='white', key_length="100 km s**-1", density=1.0, **kwargs):
     """
 
     Make an SPH image of the given simulation with velocity vectors overlaid on top.
@@ -115,7 +115,8 @@ def velocity_image(sim, width="10 kpc", vector_color='black', edgecolor='black',
         else:
             Q = p.quiver(X, Y, vx, vy, scale=_units.Unit(scale).in_units(
                 sim['vel'].units), color=vector_color, edgecolor=edgecolor)
-        p.quiverkey(Q, key_x, key_y, key_unit.in_units(sim['vel'].units, **sim.conversion_context()),
+        if qkey==True:
+		p.quiverkey(Q, key_x, key_y, key_unit.in_units(sim['vel'].units, **sim.conversion_context()),
                     r"$\mathbf{" + key_unit.latex() + "}$", labelcolor=key_color, color=key_color, fontproperties={'size': 16})
     elif mode == 'stream' :
         Q = p.streamplot(X, Y, vx, vy, color=vector_color, density=density)
