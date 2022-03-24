76d3da9f38b4f2a7f89c300c8982da0b1e1963cb
mtremmel <m.tremmel6@gmail.com>
2022-03-24 16:51:56 -0400

    revert back to old way of making the grid, removing new keywords (they only make images worse)

--- pynbody/plot/volume_render.py
+++ pynbody/plot/volume_render.py
@@ -44,8 +44,7 @@ class RenderVolume(object):
 			grid_data = self._loaded_data[data_name]
 		else:
 			grid_data = sph.to_3d_grid(ss, qty=qty, nx=self.resolution,snap_slice=snap_slice,
-		                           x2=None if width is None else width / 2, denoise=True,
-		                               approximate_fast=False)
+		                           x2=None if width is None else width / 2)
 			self._loaded_data[data_name] = grid_data
 		return grid_data
 
